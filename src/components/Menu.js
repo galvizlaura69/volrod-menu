6f6c6f6c1dde68cf174230c976f0cb4cb931997c
galvizlaura69 <galvizlaura69@gmail.com>
2021-10-29 08:46:31 -0500

    listas

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,20 +10,25 @@ export default function Menu() {
     <Link className="block pb-16 text-xs" to="/">
         Dash board
     </Link>
-    <div className="grid grid-cols-2 pb-9"> 
-        <div className="col-span-1">
-           <img classnema="object-cover"  src="https://firebasestorage.googleapis.com/v0/b/tienda-5bf0e.appspot.com/o/20176.png?alt=media&token=95585479-cf4c-48d1-a605-7a27ee8a3685"/> 
+    <div className="grid grid-cols-2 pb-9 items-stretch"> 
+        <div className="col-span-1 ">
+           <img className="icon-menu object-center"  src="https://firebasestorage.googleapis.com/v0/b/tienda-5bf0e.appspot.com/o/20176.png?alt=media&token=95585479-cf4c-48d1-a605-7a27ee8a3685"/> 
         </div>
-        <div className="col-span-1">
-          <Link className="block text-xs text-gray-400" to="/almacen">
-             ALMACÉN 
-          </Link>
-          <Link className="block text-xs text-gray-400" to="/vehiculos">
-             VEHÍCULOS
-         </Link>
-         <Link className="block text-xs text-gray-400" to="/sistemas">
-            SISTEMAS
-          </Link>
+        <div className="col-span-1 ">
+           <ul>
+             <li> <Link className="block text-xs text-gray-400 m-0" to="/almacen">
+                    ALMACÉN 
+                 </Link>
+              </li>
+              <li> <Link className="block text-xs text-gray-400 m-0" to="/vehiculos">
+                     VEHÍCULOS
+                   </Link>
+              </li>     
+              <li><Link className="block text-xs text-gray-400" to="/sistemas">
+                    SISTEMAS
+                  </Link>
+              </li>    
+          </ul>
         </div>  
     </div>
 
